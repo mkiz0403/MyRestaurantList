023a3a813c8f40879c6de58968bc4afb193ca18b
Taejeong <mkiz0403@gmail.com>
2024-06-22 14:30:00 +0900

    Add : LoginPage 생성

---
+++ client/src/login/LoginPage.tsx
@@ -0,0 +1,21 @@
+function LoginPage() {
+  return (
+    <>
+      <div>
+        <div>
+          <input type="inputId" />
+        </div>
+        <div>
+          <input type="inputPassword" />
+        </div>
+        <div>
+          <input type="checkbox" />
+        </div>
+        <div>
+          <button>확인</button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default LoginPage;

ccd0c0b984d05f828109dadcad26c42a0a7e2dbd
Taejeong <mkiz0403@gmail.com>
2024-06-22 15:56:32 +0900

    Add : LoginPage 컴포넌트 구현(아이디, 패스워드, 자동입력 기능까지 완료)

--- client/src/login/LoginPage.tsx
+++ client/src/login/LoginPage.tsx
@@ -1,18 +1,139 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const [saveLoginInfo, setSaveLoginInfo] = useState(false);
+
+  async function handleLogin(e: { preventDefault: () => void }) {
+    e.preventDefault();
+
+    if (!userId) {
+      alert("등록된 아이디가 아닙니다.");
+      return;
+    }
+
+    if (!password) {
+      alert("패스워드가 일치하지 않습니다.");
+      return;
+    }
+
+    try {
+      console.log("로그인 성공!");
+      console.log(userId, password);
+      if (saveLoginInfo) {
+        localStorage.setItem("userId", userId);
+        localStorage.setItem("password", password);
+      } else {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("password");
+      }
+      console.log(saveLoginInfo);
+    } catch (error) {
+      console.error("로그인 실패");
+    }
+  }
+
   return (
     <>
-      <div>
-        <div>
-          <input type="inputId" />
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Login
+          </h2>
         </div>
-        <div>
-          <input type="inputPassword" />
-        </div>
-        <div>
-          <input type="checkbox" />
-        </div>
-        <div>
-          <button>확인</button>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="userId"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                ID
+              </label>
+              <div className="mt-2">
+                <input
+                  id="userId"
+                  name="userId"
+                  type="text"
+                  value={userId}
+                  onChange={(e) => setUserId(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="#" className="text-">
+                자동입력
+              </label>
+              <input
+                id="checkbox"
+                name="saveLoginInfo"
+                type="checkbox"
+                value="saveLoginInfo"
+                onChange={() => setSaveLoginInfo(!saveLoginInfo)}
+                required
+                className=""
+              />
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                onClick={handleLogin}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                로그인 하기
+              </button>
+            </div>
+            <div className="text-sm"></div>
+          </form>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            멤버가 아니시간가요?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              지금 당장 회원 가입하세요
+            </a>
+          </p>
         </div>
       </div>
     </>

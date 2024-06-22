04fa98602cda2e676f4b73834b34f727af8bb205
Taejeong <mkiz0403@gmail.com>
2024-06-22 17:32:22 +0900

    add : router 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import LoginPage from "./login/LoginPage";
+import SignUp from "./login/SignUp";
 
 function App() {
   return (
     <>
-      <p> Hello world!</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- client/src/login/LoginPage.tsx
+++ client/src/login/LoginPage.tsx
@@ -1,14 +1,24 @@
+import "./LgoinPage.css";
 import { useState } from "react";
+import { Link, Link as RouterLink, useNavigate } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 
 function LoginPage() {
-  const [userId, setUserId] = useState("");
+  const navigate = useNavigate();
+
+  const [userEmail, setUserEmail] = useState("");
   const [password, setPassword] = useState("");
   const [saveLoginInfo, setSaveLoginInfo] = useState(false);
 
   async function handleLogin(e: { preventDefault: () => void }) {
     e.preventDefault();
 
-    if (!userId) {
+    if (!userEmail) {
       alert("등록된 아이디가 아닙니다.");
       return;
     }
@@ -20,14 +30,15 @@ function LoginPage() {
 
     try {
       console.log("로그인 성공!");
-      console.log(userId, password);
+      console.log(userEmail, password);
       if (saveLoginInfo) {
-        localStorage.setItem("userId", userId);
+        localStorage.setItem("userId", userEmail);
         localStorage.setItem("password", password);
       } else {
         localStorage.removeItem("userId");
         localStorage.removeItem("password");
       }
+      navigate("/");
       console.log(saveLoginInfo);
     } catch (error) {
       console.error("로그인 실패");
@@ -35,108 +46,60 @@ function LoginPage() {
   }
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+    <div className="loginMain">
+      <Box component="form" noValidate autoComplete="off">
+        <div>
+          <h1>로그인</h1>
+        </div>
+        <div>
+          <TextField
+            required
+            id="userEmail"
+            label="유저 이메일"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            sx={{ mb: "1rem", width: "20rem" }}
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Login
-          </h2>
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="userId"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                ID
-              </label>
-              <div className="mt-2">
-                <input
-                  id="userId"
-                  name="userId"
-                  type="text"
-                  value={userId}
-                  onChange={(e) => setUserId(e.target.value)}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="#" className="text-">
-                자동입력
-              </label>
-              <input
-                id="checkbox"
-                name="saveLoginInfo"
-                type="checkbox"
-                value="saveLoginInfo"
+        <div>
+          <TextField
+            required
+            id="password"
+            label="패스트워드"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            sx={{ mb: "0.5rem", width: "20rem" }}
+          />
+        </div>
+        <div className="checkbox">
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="remember"
+                color="primary"
+                checked={saveLoginInfo}
                 onChange={() => setSaveLoginInfo(!saveLoginInfo)}
-                required
-                className=""
               />
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                onClick={handleLogin}
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                로그인 하기
-              </button>
-            </div>
-            <div className="text-sm"></div>
-          </form>
-          <p className="mt-10 text-center text-sm text-gray-500">
-            멤버가 아니시간가요?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              지금 당장 회원 가입하세요
-            </a>
-          </p>
+            }
+            label="자동 입력"
+          />
         </div>
-      </div>
-    </>
+        <div>
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={handleLogin}
+            sx={{ mb: "1rem", width: "20rem" }}
+          >
+            로그인 하기
+          </Button>
+        </div>
+        <div className="signup">
+          <p> 회원이 아니시라면 👉</p>
+          <Link to={"./signup"}> 회원 가입하기 </Link>
+        </div>
+      </Box>
+    </div>
   );
 }
 export default LoginPage;

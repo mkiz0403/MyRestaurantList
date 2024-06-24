4088b719d934eb787131ea7c51a9cd0297356e16
Taejeong <mkiz0403@gmail.com>
2024-06-24 15:02:29 +0900

    modfiy : 프리티어 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-import LoginPage from "./login/LoginPage";
-import SignUp from "./login/SignUp";
-import Home from "./Home";
+import LoginPage from './login/LoginPage';
+import SignUp from './login/SignUp';
+import Home from './Home';
 
 function App() {
   return (

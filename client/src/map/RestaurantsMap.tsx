75a60735b38d70f96eceb7405d452ac553071e88
Taejeong <mkiz0403@gmail.com>
2024-06-24 12:50:08 +0900

    Create : RestaurantsMap Compoenet

---
+++ client/src/map/RestaurantsMap.tsx
@@ -0,0 +1,12 @@
+import './RestaurantsMap.css';
+
+function RestaurantsMap() {
+  return (
+    <div
+      style={{ width: '70%', backgroundColor: 'red', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <h1>Map</h1>
+    </div>
+  );
+}
+export default RestaurantsMap;

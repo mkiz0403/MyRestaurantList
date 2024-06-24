f327f473359ecaa7fa5f0892c3d637b3c2861591
Taejeong <mkiz0403@gmail.com>
2024-06-25 00:15:42 +0900

    add : 등록된 플레이스 마커 표시, 플에이스 정보에서 주소 클릭시 주소 위치로 이동.

--- client/src/map/RestaurantsMap.tsx
+++ client/src/map/RestaurantsMap.tsx
@@ -1,49 +1,110 @@
 import './RestaurantsMap.css';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
 declare global {
   interface Window {
     naver: any;
   }
 }
 
-function RestaurantsMap() {
+interface Restaurant {
+  imageUrl: string;
+  foodType: string;
+  placeName: string;
+  review: string;
+  address: string;
+  visitsCount: number;
+}
+
+interface MapProps {
+  places: Restaurant[];
+  selectedAddress: string | null;
+}
+
+function RestaurantsMap({ places, selectedAddress }: MapProps) {
+  const mapRef = useRef<any>(null);
+
   useEffect(() => {
     const { naver } = window;
 
     if (!naver) return;
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    const initMap = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
 
-        const mapOptions = {
-          center: new naver.maps.LatLng(latitude, longitude),
-          zoom: 15,
-        };
+          const mapOptions = {
+            center: new naver.maps.LatLng(latitude, longitude),
+            zoom: 18,
+            zoomControlOptions: {
+              position: naver.maps.Position.TOP_RIGHT,
+            },
+          };
 
-        const map = new naver.maps.Map('map', mapOptions);
-        // 현재 위치 마커로 표시하기
-        const marker = new naver.maps.Marker({
-          position: new naver.maps.LatLng(latitude, longitude),
-          map: map,
-        });
+          // 현재 위치 마커로 표시하기
+          const map = new naver.maps.Map(mapRef.current, mapOptions);
 
-        // 예시로 사용자가 클릭한 위치에 마커를 추가하는 코드
-        naver.maps.Event.addListener(map, 'click', function (e: any) {
-          const marker = new naver.maps.Marker({
-            position: e.coord,
+          new naver.maps.Marker({
+            position: new naver.maps.LatLng(latitude, longitude),
             map: map,
           });
-        });
-      },
-      (error) => {
-        console.error('Error getting current position:', error);
-      },
-    );
-  }, []);
+
+          places.forEach((place) => {
+            naver.maps.Service.geocode(
+              {
+                query: place.address,
+              },
+              function (status: any, response: any) {
+                if (status !== naver.maps.Service.Status.OK) {
+                  return alert('Something went wrong!');
+                }
+                const location = response.v2.addresses[0];
+                const latlng = new naver.maps.LatLng(location.y, location.x);
+
+                new naver.maps.Marker({
+                  position: latlng,
+                  map: map,
+                  title: place.placeName,
+                });
+              },
+            );
+          });
+
+          if (selectedAddress) {
+            naver.maps.Service.geocode(
+              {
+                query: selectedAddress,
+              },
+              function (status: any, response: any) {
+                if (status !== naver.maps.Service.Status.OK) {
+                  return alert('Something went wrong!');
+                }
+                console.log(response); // 응답 객체 확인
+                const location = response.v2.addresses[0];
+                const latlng = new naver.maps.LatLng(location.y, location.x);
+
+                new naver.maps.Marker({
+                  position: latlng,
+                  map: map,
+                });
+                console.log(position);
+                map.setCenter(latlng);
+              },
+            );
+          }
+        },
+        (error) => {
+          console.error('Error getting current position:', error);
+        },
+      );
+    };
+    initMap();
+  }, [places, selectedAddress]);
+
   return (
     <div
-      id="map"
+      ref={mapRef}
       style={{
         width: '70%',
         height: '800px',

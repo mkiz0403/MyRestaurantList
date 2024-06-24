630751362cae6f3363692a4320ffcff0b9399f0c
Taejeong <mkiz0403@gmail.com>
2024-06-24 15:20:59 +0900

    modfy : 지도의 위치를 클릭했을때 주소를 가져오는 코드 삭제, 현재 위치를 표시해주는 코드 추가.

--- client/src/map/RestaurantsMap.tsx
+++ client/src/map/RestaurantsMap.tsx
@@ -12,34 +12,34 @@ function RestaurantsMap() {
 
     if (!naver) return;
 
-    const mapOptions = {
-      center: new naver.maps.LatLng(37.5112, 127.0981),
-      zoom: 15,
-    };
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-    const map = new naver.maps.Map('map', mapOptions);
+        const mapOptions = {
+          center: new naver.maps.LatLng(latitude, longitude),
+          zoom: 15,
+        };
 
-    // 예시로 사용자가 클릭한 위치에 마커를 추가하는 코드
-    naver.maps.Event.addListener(map, 'click', function (e: any) {
-      const marker = new naver.maps.Marker({
-        position: e.coord,
-        map: map,
-      });
+        const map = new naver.maps.Map('map', mapOptions);
+        // 현재 위치 마커로 표시하기
+        const marker = new naver.maps.Marker({
+          position: new naver.maps.LatLng(latitude, longitude),
+          map: map,
+        });
 
-      // 클릭한 위치의 주소를 가져오기
-      naver.maps.Service.reverseGeocode(
-        {
-          coords: e.coord,
-        },
-        function (status: any, response: any) {
-          if (status === naver.maps.Service.Status.ERROR) {
-            return alert('Something wrong!');
-          }
-          const address = response.result.items[0].address;
-          console.log(`주소는 ${address} 입니다.`); // 주소를 콘솔에 출력하거나, 원하는 곳에 표시
-        },
-      );
-    });
+        // 예시로 사용자가 클릭한 위치에 마커를 추가하는 코드
+        naver.maps.Event.addListener(map, 'click', function (e: any) {
+          const marker = new naver.maps.Marker({
+            position: e.coord,
+            map: map,
+          });
+        });
+      },
+      (error) => {
+        console.error('Error getting current position:', error);
+      },
+    );
   }, []);
   return (
     <div
@@ -47,7 +47,6 @@ function RestaurantsMap() {
       style={{
         width: '70%',
         height: '800px',
-        backgroundColor: 'red',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',

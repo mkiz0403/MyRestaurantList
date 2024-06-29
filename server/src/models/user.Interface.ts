9fc8b448ac657b1ee6028b5413bf0be0ded8b2ee
Taejeong <mkiz0403@gmail.com>
2024-06-29 15:42:48 +0900

    유저 인터페이스 생성

---
+++ server/src/models/user.Interface.ts
@@ -0,0 +1,15 @@
+export default interface UserInterface {
+  userNickName: string;
+  userEmail: string;
+  password: string;
+  userImg: string;
+  userType: string;
+  userRestaurent: {
+    imageUrl: string;
+    foodType: string;
+    placeName: string;
+    review: string;
+    address: string;
+    visitsCount: number;
+  }[];
+}

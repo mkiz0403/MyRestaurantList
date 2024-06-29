a6ba67ee62a5fc8e16f10d2c494fbb53b6fe6a5e
Taejeong <mkiz0403@gmail.com>
2024-06-29 17:12:17 +0900

    이미지 업로드 기능 삭제

--- client/src/login/SignUp.tsx
+++ client/src/login/SignUp.tsx
@@ -8,55 +8,13 @@ function SignUp() {
   const [userEmail, setUserEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
   const [userNickName, setUserNickName] = useState('');
-
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-
-  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0];
-    if (file) {
-      setSelectedImage(file);
-      setPreviewUrl(URL.createObjectURL(file));
-    }
-  }
-
-  async function handleUpload() {
-    if (!selectedImage) return;
-
-    const formData = new FormData();
-    formData.append('image', selectedImage);
-
-    try {
-      const response = await fetch('http://localhost:4000/upload', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        console.log('이미지 업로드 성공');
-      } else {
-        console.error('이미지 업로드 실패');
-      }
-    } catch (error) {
-      console.error('업로드 에러:', error);
-    }
-  }
-
-  async function handleRemove() {
-    if (!selectedImage) return;
-    setSelectedImage(null);
-    setPreviewUrl(null);
-    console.log('미리보기 이미지가 삭제되었습니다.');
-  }
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
-      console.log(`유저이메일: ${userEmail}, 유저 닉네임: ${userNickName}, 이미지 : ${selectedImage}`);
+      console.log(`유저이메일: ${userEmail}, 유저 닉네임: ${userNickName}`);
       console.log('회원가입');
-      await handleUpload();
       navigate('/');
     } catch (error) {
       console.error('에러발생');
@@ -77,30 +35,6 @@ function SignUp() {
         >
           <div>
             <h1>회원가입</h1>
-          </div>
-          <div>
-            <h1>나의 이미지</h1>
-            <input type="file" accept="image/*" onChange={handleImageChange} />
-            {previewUrl && (
-              <div>
-                <div>
-                  <img
-                    style={{
-                      width: '200px',
-                      height: 'auto',
-                      borderRadius: '50%',
-                    }}
-                    src={previewUrl}
-                    alt="Preview"
-                  />
-                </div>
-                <div>
-                  <Button variant="contained" onClick={handleRemove} sx={{ mt: 2 }}>
-                    x
-                  </Button>
-                </div>
-              </div>
-            )}
           </div>
           <Box component="form" noValidate sx={{ mt: 3 }}>
             <FormControl component="fieldset" variant="standard">

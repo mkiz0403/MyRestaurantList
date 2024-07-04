47196475da4adb136eb1f58f05c211a3c17ba8ab
Taejeong <mkiz0403@gmail.com>
2024-07-04 16:18:45 +0900

    유저 인터페이스 수정 : 필수 타입이 아닌 것들 처리

--- server/src/models/user.Interface.ts
+++ server/src/models/user.Interface.ts
@@ -2,14 +2,14 @@ export default interface UserInterface {
   userNickName: string;
   userEmail: string;
   password: string;
-  userImg: string;
+  userImg?: string;
   userType: string;
-  userRestaurent: {
-    imageUrl: string;
-    foodType: string;
-    placeName: string;
-    review: string;
-    address: string;
-    visitsCount: number;
+  userRestaurent?: {
+    imageUrl?: string;
+    foodType?: string;
+    placeName?: string;
+    review?: string;
+    address?: string;
+    visitsCount?: number;
   }[];
 }

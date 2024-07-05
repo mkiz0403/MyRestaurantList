8b95ad6589c72da5dfe30515a1c069f53cb503e2
Taejeong <mkiz0403@gmail.com>
2024-07-05 15:29:16 +0900

    login, getUser api에 token 체크 적용

--- client/api/userRestaurantApi.ts
+++ client/api/userRestaurantApi.ts
@@ -4,13 +4,16 @@ const apiUrl = 'http://127.0.0.1:4000/';
 
 const userRestaurentAxios = axios.create({ baseURL: apiUrl });
 
-export async function login(userEmail: string, password: string): Promise<UserInterface | undefined> {
+export async function login(
+  userEmail: string,
+  password: string,
+): Promise<{ token: string; user: UserInterface } | undefined> {
   try {
     const res = await userRestaurentAxios.post('/login', { userEmail, password });
     console.log('서버 응답:', res.data);
-    const { user } = res.data;
+    const { token, user } = res.data;
     if (user.userEmail === userEmail) {
-      return user;
+      return { token, user };
     } else {
       throw new Error('유저 이메일이 일치하지 않습니다.');
     }
@@ -25,16 +28,20 @@ export async function signup(
   userNickName: string,
 ): Promise<UserInterface | undefined> {
   try {
-    const res = await userRestaurentAxios.post('./signup', { userEmail, password, userNickName });
+    const res = await userRestaurentAxios.post('/signup', { userEmail, password, userNickName });
     return res.data;
   } catch (error) {
     console.error('에러발생');
   }
 }
 
-export async function getUser(userEmail: string): Promise<UserInterface | undefined> {
+export async function getUser(userEmail: string, token: string): Promise<UserInterface | undefined> {
   try {
-    const res = await userRestaurentAxios.get(`./${userEmail}`);
+    const res = await userRestaurentAxios.get(`/${userEmail}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return res.data;
   } catch (error) {
     console.error('유저 정보를 가져올 수 없습니다.');

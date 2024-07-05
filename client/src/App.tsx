34dbebb4f767384ebaa43a129948e448f3ada6e4
Taejeong <mkiz0403@gmail.com>
2024-07-05 15:32:27 +0900

    home 라우터 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
-          <Route path="/" element={<Home />} />
+          <Route path="/:userEmail" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>

fe10ee927a116388e9a2e4219d6a0747237a0df7
Taejeong <mkiz0403@gmail.com>
2024-07-05 13:58:37 +0900

    백엔드 api 추가 : 유저 정보 불러오는 api, 유저 정보 수정하는 api

--- server/src/index.ts
+++ server/src/index.ts
@@ -57,12 +57,49 @@ app.post('/signup', async (req, res) => {
 
     if (!newUser?.userEmail) {
       return res.status(400).send({ message: '동일한 이메일이 존재합니다.' });
-    } else if(!newUser?.userNickName) {
-      return res.status(400).send({message : '동일한 닉네임이 존재합니다.'})
+    } else if (!newUser?.userNickName) {
+      return res.status(400).send({ message: '동일한 닉네임이 존재합니다.' });
     }
     res.status(200).send({ message: '회원가입 성공!', userEmail: userEmail, userNickName: userNickName });
   } catch (error) {
     console.error('회원가입에 실패 했습니다.');
+  }
+});
+
+app.get('/:userEmail', async (req, res) => {
+  const { userEmail } = req.params;
+
+  try {
+    const user = await userFileSystem.getUser(userEmail);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+    }
+  } catch (error) {
+    console.error('정보를 가져오지 못했습니다.', error);
+    res.status(500).json({ message: '유저 정보를 가져오는 중 오류가 발생했습니다.' });
+  }
+});
+
+app.put('/update/:userEmail', async (req, res) => {
+  const { userEmail, password, userNickName, userImg } = req.body;
+
+  try {
+    const updatedUser = await userFileSystem.userUpdate(userEmail, {
+      password,
+      userNickName,
+      userImg,
+    } as UserInterface);
+
+    if (updatedUser) {
+      res.status(200).json({ message: '업데이트가 성공했습니다.', user: updatedUser });
+    } else {
+      res.status(400).json({ message: '유저 정보를 찾을 수 없습니다.' });
+    }
+  } catch (error) {
+    console.error('업데이트 실패');
+    res.status(500).json({ message: '업데이트 중 오류 발생!!!' });
   }
 });
 

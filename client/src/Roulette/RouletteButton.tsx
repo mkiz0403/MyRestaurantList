d8f193878f54a4386d6bb1bfae6ca6923489aea4
Taejeong <mkiz0403@gmail.com>
2024-07-27 15:48:40 +0900

    add : 룰렛돌리는 함수, 룰렛결과 보여주는 컴포넌트 추가

--- client/src/Roulette/RouletteButton.tsx
+++ client/src/Roulette/RouletteButton.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import RouletteStoreAdd from './RouletteStoreAdd';
+import RouletteStoreAddPopup from './RouletteStoreAddPopup';
+import ResultStore from './ResultStore';
+import { UserStore } from '../models/user.interface';
 
-function RouletteButton() {
-  const [openRoulette, setOpenRoulette] = useState(false);
+interface RouletteButtonProps {
+  userStore: UserStore[];
+  stores: string[];
+  setStore: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+function RouletteButton({ stores, setStore, userStore }: RouletteButtonProps) {
+  const [openRoulette, setOpenRoulette] = useState(true);
+  const [result, setResult] = useState<string | null>(null);
+  const [showResult, setShowResult] = useState(false);
 
   function handleRouletteON() {
     setOpenRoulette(true);
@@ -11,12 +21,35 @@ function RouletteButton() {
 
   function handleRouletteOff() {
     setOpenRoulette(false);
+    setStore([]);
+  }
+
+  function handleResultClose() {
+    setShowResult(false);
+    setResult(null);
+    setStore([]);
+  }
+
+  function spinRoulette() {
+    if (stores.length > 0) {
+      const randomIndex = Math.floor(Math.random() * stores.length);
+      setResult(stores[randomIndex]);
+      setShowResult(true);
+      setOpenRoulette(false);
+    } else {
+      alert('룰렛에 음식점을 추가해 주세요.');
+    }
   }
 
   return (
     <>
       <Button onClick={handleRouletteON}>맛집 뽑기 😋</Button>
-      {openRoulette && <RouletteStoreAdd onOpen={handleRouletteON} onClose={handleRouletteOff} />}
+      {openRoulette && (
+        <RouletteStoreAddPopup stores={stores} setStore={setStore} onClose={handleRouletteOff} onSpin={spinRoulette} />
+      )}
+      {showResult && (
+        <ResultStore result={result} spinRoulette={spinRoulette} onClose={handleResultClose} userStore={userStore} />
+      )}
     </>
   );
 }
@@ -32,7 +65,8 @@ const Button = styled.button`
   font-size: 18px;
   color: white;
   position: absolute;
-  top: 10px;
+  top: 1rem;
   right: 8rem;
   z-index: 10;
+  position: fixed;
 `;
